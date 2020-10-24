93d1e36edf31e6c5f4b59a724d5c903ca55de105
suhitpatil <suhit.patil@gmail.com>
2020-10-25 00:17:47 +0530

    Initial Commit

---
+++ MovieDB-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieDB-SwiftUI
+//
+//  Created by Suhit Patil on 25/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieDB-SwiftUI/MovieDB_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDB_SwiftUIApp.swift
+//  MovieDB-SwiftUI
+//
+//  Created by Suhit Patil on 25/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MovieDB_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
